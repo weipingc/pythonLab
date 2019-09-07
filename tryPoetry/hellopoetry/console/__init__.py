8718be58d4fc9ffa8209c6ec497630225a9dc50a
Wei <Wei@Weipings-Air.zyxel.com>
2019-09-07 15:02:23 -0500

    initial version

---
+++ tryPoetry/hellopoetry/console/__init__.py
@@ -0,0 +1,4 @@
+from .application import Application
+
+def main():
+    return Application().run()
---
+++ tryPoetry/hellopoetry/console/application.py
@@ -0,0 +1,6 @@
+class Application():
+    def __init__(self):
+        print("[Application.__init__]")
+
+    def run(self):
+        print("[Application.run] Hi App by Poetry!")
