bddea9a493558fda0de5830f8425c84c03667b5d
Wei <Wei@Weipings-Air.zyxel.com>
2019-09-07 21:19:46 -0500

    Added code to test adding dependency numpy; Added .gitignore

--- tryPoetry/hellopoetry/console/application.py
+++ tryPoetry/hellopoetry/console/application.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 class Application():
     def __init__(self):
         print("[Application.__init__]")
 
     def run(self):
         print("[Application.run] Hi App by Poetry!")
+        fiveZeros = np.zeros(5)
+        print("fiveZeros=", fiveZeros)
+
